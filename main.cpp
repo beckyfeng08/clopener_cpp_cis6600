1fde3ea601dc4a30daf55683ded82ac1f5b347d2
beckyfeng08 <beckyfeng08@berkeley.edu>
2026-03-29 16:35:59 -0400

    add the remesh botsch

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include "remesh_botsch.h"
+#include <igl/boundary_loop.h>
+#include "all_boundary_loop.h"
+#include <Eigen/Core>
+// #include <igl/read_triangle_mesh.h>
+// #include <igl/write_triangle_mesh.h>
 
-int main(int argc, char *argv[])
-{
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
+
+void create_cube(Eigen::MatrixXd &V, Eigen::MatrixXi &F) {
+V= (Eigen::MatrixXd(8,3)<<
     0.0,0.0,0.0,
     0.0,0.0,1.0,
     0.0,1.0,0.0,
@@ -12,7 +17,7 @@ int main(int argc, char *argv[])
     1.0,0.0,1.0,
     1.0,1.0,0.0,
     1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
+  F = (Eigen::MatrixXi(12,3)<<
     0,6,4,
     0,2,6,
     0,3,2,
@@ -25,7 +30,36 @@ int main(int argc, char *argv[])
     0,5,1,
     1,5,7,
     1,7,3).finished();
+}
+int main(int argc, char *argv[])
+{
+  //  ~~~~~ STEP 0: LOAD MESH ~~~~~
+  // Inline mesh of a cube
 
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+  // generates a cube mesh and populates V and F
+  create_cube(V, F);
+
+  // uncomment to generate bunny mesh or any arbitrary mesh
+  // igl::read_triangle_mesh(argc>1 ? argv[1] : "../data/bunny.off", V, F);
+
+  // uncomment for read .obj files, be sure to uncomment headers too
+      // igl::read_triangle_mesh(in,V,F);
+
+  //   ~~~~~ STEP 1: remesh (copied and pasted from botsch-kobbelt-remesher-libigl/remeshmesh.cpp) ~~~~~
+
+    bool project = false;
+    int iterations = 10;
+    double h = 0.05;
+    
+    Eigen::VectorXd target = Eigen::VectorXd::Constant(V.rows(),h);
+    Eigen::VectorXi feature;
+	feature.resize(0);
+    remesh_botsch(V,F,target,iterations, feature, project); // V, F, and target are taken 
+  // all_boundary_loop(F);
+
+  //  ~~~~~ STEP 2: VIEW MESH ~~~~~
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
   viewer.data().set_mesh(V, F);
